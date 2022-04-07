f33abd69817dfa4e4bce10f8fa31457fadf3fa70
ipowerful <eley81@gmail.com>
2022-04-07 16:59:29 +0300

    Update layout

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -60,91 +60,11 @@ export default {
 	font-display: swap;
 }
 
-
-//@font-face {
-//  font-family: 'FoglihtenNo06';
-//  src: url('/fonts/FoglihtenNo06.eot');
-//  src:
-//      url('/fonts/FoglihtenNo06.eot?#iefix') format('embedded-opentype'),
-//      url('/fonts/Magnat-HeadRegular.woff2') format('woff2'),
-//      url('/fonts/Magnat-HeadRegular.woff') format('woff'),
-//      url('/fonts/Magnat-HeadRegular.ttf') format('truetype');
-//  font-weight: normal;
-//  font-style: normal;
-//  font-display: swap;
-//}
-
-//@font-face {
-//	font-family: 'TT Norms Pro';
-//	src: url('~assets/fonts/TTNormsPro-Thin.eot');
-//	src: url('~assets/fonts/TTNormsPro-Thin.eot?#iefix') format('embedded-opentype'),
-//	url('~assets/fonts/TTNormsPro-Thin.woff2') format('woff2'),
-//	url('~assets/fonts/TTNormsPro-Thin.woff') format('woff'),
-//	url('~assets/fonts/TTNormsPro-Thin.ttf') format('truetype');
-//	font-weight: 100;
-//	font-style: normal;
-//	font-display: swap;
-//}
-
-//@font-face {
-//	font-family: 'TT Norms Pro';
-//	src: url('~assets/fonts/TTNormsPro-Light.eot');
-//	src: local('TT Norms Pro Light'), local('TTNormsPro-Light'),
-//	url('~assets/fonts/TTNormsPro-Light.eot?#iefix') format('embedded-opentype'),
-//	url('~assets/fonts/TTNormsPro-Light.woff2') format('woff2'),
-//	url('~assets/fonts/TTNormsPro-Light.woff') format('woff'),
-//	url('~assets/fonts/TTNormsPro-Light.ttf') format('truetype');
-//	font-weight: 300;
-//	font-style: normal;
-//	font-display: swap;
-//}
-//
-//@font-face {
-//	font-family: 'TT Norms Pro';
-//	src: url('~assets/fonts/TTNormsPro-Regular.eot');
-//	src: url('~assets/fonts/TTNormsPro-Regular.eot?#iefix') format('embedded-opentype'),
-//	url('~assets/fonts/TTNormsPro-Regular.woff2') format('woff2'),
-//	url('~assets/fonts/TTNormsPro-Regular.woff') format('woff'),
-//	url('~assets/fonts/TTNormsPro-Regular.ttf') format('truetype');
-//	font-weight: normal;
-//	font-style: normal;
-//	font-display: swap;
-//}
-//
-//@font-face {
-//	font-family: 'TT Norms Pro';
-//	src: url('~assets/fonts/TTNormsPro-Medium.eot');
-//	src: local('TT Norms Pro Medium'), local('TTNormsPro-Medium'),
-//	url('~assets/fonts/TTNormsPro-Medium.eot?#iefix') format('embedded-opentype'),
-//	url('~assets/fonts/TTNormsPro-Medium.woff2') format('woff2'),
-//	url('~assets/fonts/TTNormsPro-Medium.woff') format('woff'),
-//	url('~assets/fonts/TTNormsPro-Medium.ttf') format('truetype');
-//	font-weight: 500;
-//	font-style: normal;
-//	font-display: swap;
-//}
-//
-//@font-face {
-//	font-family: 'TT Norms Pro';
-//	src: url('~assets/fonts/TTNormsPro-Bold.eot');
-//	src: url('~assets/fonts/TTNormsPro-Bold.eot?#iefix') format('embedded-opentype'),
-//	url('~assets/fonts/TTNormsPro-Bold.woff2') format('woff2'),
-//	url('~assets/fonts/TTNormsPro-Bold.woff') format('woff'),
-//	url('~assets/fonts/TTNormsPro-Bold.ttf') format('truetype');
-//	font-weight: bold;
-//	font-style: normal;
-//	font-display: swap;
-//}
-
 body {
 	min-width: $body-min-width;
 	font: rem(18px)/rem(30px) $ff-base;
 
-	background: #F0D088;
+	background: $color-yellow;
 	color: $color-base;
-}
-
-.layout {
-
 }
 </style>
