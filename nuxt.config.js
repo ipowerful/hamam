81f977772736827656876dd3ae15c922c60b6c0f
ipowerful <eley81@gmail.com>
2022-04-07 18:39:02 +0300

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const FRONT_URL = process.env.FRONT_URL
+
 export default {
 	target: 'static',
 	head: {
@@ -10,7 +12,55 @@ export default {
 			{ name: 'viewport', content: 'width=device-width' },
 			{ hid: 'description', name: 'description', content: '' },
 			{ name: 'format-detection', content: 'telephone=no' },
+			{
+				name: 'msapplication-TileColor',
+				content: '#da532c',
+			}, {
+				name: 'msapplication-config',
+				content: `${FRONT_URL}/favicons/browserconfig.xml`,
+			}, {
+				name: 'theme-color',
+				content: '#ffffff',
+			}, {
+				hid: 'og:type',
+				property: 'og:type',
+				content: 'website',
+			}, {
+				hid: 'og:image',
+				property: 'og:image',
+				content: `${FRONT_URL}/favicons/og-image.png`,
+			},
+			{
+				hid: 'og:url',
+				property: 'og:url',
+				content: FRONT_URL,
+			},
 		],
+		link: [{
+			rel: 'apple-touch-icon',
+			href: `${FRONT_URL}/favicons/apple-touch-icon.png`,
+			sizes: '180x180',
+		}, {
+			rel: 'icon',
+			type: 'image/png',
+			href: `${FRONT_URL}/favicons/favicon-32x32.png`,
+			sizes: '32x32',
+		}, {
+			rel: 'icon',
+			type: 'image/png',
+			href: `${FRONT_URL}/favicons/favicon-16x16.png`,
+			sizes: '16x16',
+		}, {
+			rel: 'manifest',
+			href: `${FRONT_URL}/favicons/site.webmanifest`,
+		}, {
+			rel: 'mask-icon',
+			href: `${FRONT_URL}/favicons/safari-pinned-tab.svg`,
+			color: '#5bbad5',
+		}, {
+			rel: 'shortcut icon',
+			href: `${FRONT_URL}/favicons/favicon.ico`,
+		}],
 	},
 	loading: {
 		color: '#F8002E',
