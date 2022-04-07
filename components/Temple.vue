f22a2c843699b397dac2387377a26f67443407fc
ipowerful <eley81@gmail.com>
2022-04-07 17:52:26 +0300

    Create Temple component

---
+++ components/Temple.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="temple">
+		<div class="container">
+			<h2 class="h2">
+				Уникальное место, <br>
+				где вы можете
+				<span>
+					отдохнуть <br>
+					душой и телом
+				</span>
+			</h2>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.temple {
+	height: 450px;
+
+	@include flex-center;
+	text-align: center;
+
+	background: #1D144C url('~assets/svg/temple.svg') center bottom ;
+
+	.h2 {
+		margin-top: 50px;
+		@include mob-h2;
+	}
+}
+</style>
