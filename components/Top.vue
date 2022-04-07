e49619b81e2fd37bc54cef9ff8a6a78392f6dc62
ipowerful <eley81@gmail.com>
2022-04-07 15:26:25 +0300

    Update Top component

--- components/Top.vue
+++ components/Top.vue
@@ -1,10 +1,18 @@
 <template>
 	<div class="top">
 		<div class="container">
-
 			<h1 class="h1">Хамам</h1>
 			<div class="text">
 				мы предлагаем вам посетить роскошную парную с высокой влажностью
+			</div>
+			<div
+				v-for="(text, i) in features"
+				:key="i"
+
+				class="feature">
+				<div class="feature-body">
+					{{ text }}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -12,38 +20,30 @@
 
 <script>
 export default {
-	// computed: {
-	// 	backgroundStyles() {
-	// 		const imgUrl = this.$img('top-bg.jpg', { width: 720, quality: 100 })
-	// 		return {
-	// 			backgroundImage: `url('${imgUrl}')`,
-	// 		}
-	// 	},
-	// },
+	data: () => ({
+		features: [
+			'Очищение тела от токсинов и шлаков',
+			'Улучшение работы сердца и сосудов',
+			'Незабываемые ощущения и потрясающее удовольствие',
+		],
+	}),
 }
 </script>
 
 <style lang="scss">
 .top {
-	height: 700px;
-	position: relative;
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: auto 100%;
-
-	background: url('~assets/images/top-bg-xs@2x.jpg') 100% / cover;
-
+	padding-bottom: 85px;
 	color: $white;
 
-	&:before,
 	&:after {
 		content: '';
-		@include absolute-contain;
+		@include abs;
+		position: fixed;
+		height: 100vh;
+		z-index: -1;
+		background: url('~assets/images/top-bg-xs@2x.jpg') 100% / cover;
 	}
 
-	//	&:after {
-	///		background: url('~assets/svg/top-overlay.svg') 100% / cover;
-	//	}
 	.h1 {
 		display: inline-block;
 		@include mob-h1;
@@ -52,6 +52,7 @@ export default {
 		margin-bottom: 43px;
 
 		position: relative;
+
 		&:before {
 			position: absolute;
 			top: -34px;
@@ -75,6 +76,50 @@ export default {
 
 	.text {
 		font: 300 rem(24px)/rem(30px) $ff-base;
+		margin-bottom: 170px;
+	}
+
+	.holder {
+		height: 320px;
+		overflow-y: scroll;
+
+		@include hide-scrollbar;
+	}
+
+	.feature {
+		font: rem(18px)/rem(26px) $ff-base;
+		position: relative;
+		@include flex-center;
+		margin-bottom: 75px;
+
+		&:before,
+		&:after {
+			content: '';
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			border-radius: 50%;
+			transform: translate(-50%, -50%);
+		}
+
+		&:before {
+			width: 322px;
+			height: 322px;
+			border: 1px solid rgba(240, 208, 136, 0.2);
+		}
+
+		&:after {
+			width: 250px;
+			height: 250px;
+			border: 1px solid $white;
+		}
+
+		&-body {
+			width: 140px;
+			height: 124px;
+
+			@include flex-center;
+		}
 	}
 }
 </style>
