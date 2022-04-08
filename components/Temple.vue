e8729d539e1ce6867a68bceb41091d0f7cd0f96d
ipowerful <eley81@gmail.com>
2022-04-08 21:07:25 +0300

    Temple: update animation type

--- components/Temple.vue
+++ components/Temple.vue
@@ -63,64 +63,29 @@ export default {
 	}
 
 	.temple-svg {
-		.svg-elem-1 {
-			stroke-dashoffset: 2630px;
-			stroke-dasharray: 2630px;
-			transition: stroke-dashoffset 0.5s cubic-bezier(0.47, 0, 0.745, 0.715) 0s;
-		}
-
-		&.active .svg-elem-1 {
-			stroke-dashoffset: 0;
-		}
-
-		.svg-elem-2 {
-			stroke-dashoffset: 2486.52734375px;
-			stroke-dasharray: 2486.52734375px;
-			transition: stroke-dashoffset 0.5s cubic-bezier(0.47, 0, 0.745, 0.715) 0.12s;
-		}
-
-		&.active .svg-elem-2 {
-			stroke-dashoffset: 0;
-		}
-
-		.svg-elem-3 {
-			stroke-dashoffset: 2091.560546875px;
-			stroke-dasharray: 2091.560546875px;
-			transition: stroke-dashoffset 0.5s cubic-bezier(0.47, 0, 0.745, 0.715) 0.24s;
-		}
-
-		&.active .svg-elem-3 {
-			stroke-dashoffset: 0;
-		}
-
-		.svg-elem-4 {
-			stroke-dashoffset: 1705.0479736328125px;
-			stroke-dasharray: 1705.0479736328125px;
-			transition: stroke-dashoffset 0.5s cubic-bezier(0.47, 0, 0.745, 0.715) 0.36s;
-		}
-
-		&.active .svg-elem-4 {
-			stroke-dashoffset: 0;
+		[class^="svg-elem-"] {
+			transition: all .5s linear 0s;
+			opacity: 0;
 		}
 
 		.svg-elem-5 {
-			stroke-dashoffset: 1322.76220703125px;
-			stroke-dasharray: 1322.76220703125px;
-			transition: stroke-dashoffset 0.5s cubic-bezier(0.47, 0, 0.745, 0.715) 0.48s;
+			transition-delay: 0.33s;
 		}
 
-		&.active .svg-elem-5 {
-			stroke-dashoffset: 0;
+		.svg-elem-4 {
+			transition-delay: 0.66s;
 		}
 
-		.svg-elem-6 {
-			stroke-dashoffset: 984.8401489257812px;
-			stroke-dasharray: 984.8401489257812px;
-			transition: stroke-dashoffset 0.5s cubic-bezier(0.47, 0, 0.745, 0.715) 0.6s;
+		.svg-elem-3 {
+			transition-delay: 0.99s;
 		}
 
-		&.active .svg-elem-6 {
-			stroke-dashoffset: 0;
+		.svg-elem-2 {
+			transition-delay: 1.32s;
+		}
+
+		&.active [class^="svg-elem-"] {
+			opacity: 1;
 		}
 	}
 
