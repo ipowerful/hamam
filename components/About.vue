d170dc0a67d601020e5a0337b3babe7d4c9cebb1
ipowerful <eley81@gmail.com>
2022-04-09 22:21:41 +0300

    About: clean up

--- components/About.vue
+++ components/About.vue
@@ -1,36 +1,5 @@
 <template>
 	<div class="about">
-		<!--		<div class="picture-top">-->
-		<!--			<nuxt-picture-->
-		<!--				class="picture"-->
-		<!--				loading="lazy"-->
-		<!--				src="about-bg@2x.jpg"-->
-		<!--				sizes="xs:100vw sm:100vw md:100vw lg:100vw xl:100vw xxl:100vw wide:100vw" />-->
-
-
-		<!--			<div-->
-		<!--				v-parallax="0.2"-->
-		<!--				class="bg flowers1" />-->
-		<!--			<div-->
-		<!--				v-parallax="0.3"-->
-		<!--				class="bg flowers2" />-->
-		<!--			<div-->
-		<!--				v-parallax="0.05"-->
-		<!--				class="bg flowers3" />-->
-
-		<!--			<nuxt-picture-->
-		<!--				v-parallax="0.01"-->
-		<!--				class="gate"-->
-		<!--				width="248px"-->
-		<!--				height="279px"-->
-		<!--				loading="lazy"-->
-		<!--				src="about-top-gate@2x.png"-->
-		<!--				sizes="xs:100vw sm:100vw md:100vw lg:100vw xl:100vw xxl:100vw wide:100vw" />-->
-		<!--			<div-->
-		<!--				v-parallax="0.01"-->
-		<!--				class="gate-shape" />-->
-		<!--			<clip-path-svg />-->
-		<!--		</div>-->
 		<div class="text">
 			<heading>
 				Хамам - особый ритуал
@@ -54,9 +23,11 @@
 				loading="lazy"
 				width="360"
 				height="400"
-				src="about-3@2x.jpg"
+				src="about@2x.jpg"
 				sizes="xs:100vw sm:100vw md:100vw lg:100vw xl:100vw xxl:100vw wide:100vw" />
-			<h2 class="h2">
+			<h2
+				v-parallax="0.1"
+				class="h2">
 				Уникальное место, <br>
 				где вы можете
 				<span>
@@ -90,12 +61,10 @@
 
 <script>
 import Heading from '@/components/Heading.vue'
-import ClipPathSvg from '@/assets/svg/about-clip-path.svg?inline'
 
 export default {
 	components: {
 		Heading,
-		ClipPathSvg,
 	},
 }
 </script>
@@ -104,73 +73,10 @@ export default {
 .about {
 	background: $color-yellow;
 
-	#aboutClipPath {
-		transform: scale(0.0028, 0.0032);
-	}
-
-	//.picture-top {
-	//	margin-bottom: 100px;
-	//	position: relative;
-	//
-	//	.picture img {
-	//		clip-path: url(#aboutClipPath)
-	//	}
-	//
-	//	.bg {
-	//		position: absolute;
-	//		z-index: 2;
-	//	}
-	//
-	//	.flowers1 {
-	//		left: 0;
-	//		top: 39%;
-	//		width: 99px;
-	//		height: 159px;
-	//		background: url('~assets/images/flowers-bg4.png') left center no-repeat;
-	//	}
-	//
-	//	.flowers2 {
-	//		left: 15.5%;
-	//		top: 83%;
-	//		width: 115px;
-	//		height: 108px;
-	//		background: url('~assets/images/flowers-bg5.png') left bottom no-repeat;
-	//	}
-	//
-	//	.flowers3 {
-	//		left: 64.5%;
-	//		top: -8%;
-	//		width: 65px;
-	//		height: 62px;
-	//		background: url('~assets/images/flowers-bg6.png') right top no-repeat;
-	//	}
-	//
-	//	.gate {
-	//		position: absolute;
-	//		z-index: 3;
-	//		left: 15.5%;
-	//		top: 29%;
-	//	}
-	//
-	//	.gate-shape {
-	//		position: absolute;
-	//		left: 18.5%;
-	//		top: 32%;
-	//		width: 249px;
-	//		height: 280px;
-	//		background: url('~assets/svg/about-gate-shape.svg') no-repeat;
-	//	}
-	//}
-
 	.picture {
 		img {
 			@include img-fluid;
 		}
-	}
-
-	.h3 {
-		@include mob-h3;
-		color: #0D0054;
 	}
 
 	p {
@@ -193,7 +99,7 @@ export default {
 		bottom: 0;
 		width: 100%;
 		text-align: center;
-		padding: 35px;
+		padding: 50px 15px;
 		@include mob-h2;
 
 		&:before {
